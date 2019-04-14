504c43985bab17c027e1e0f796f82666a73468bc
Danny Eck <danny.eck@osi.ca.gov>
2019-04-14 00:35:21 -0700

    Having some success...
    Star the docker standalone image with
    
    ```
    docker run --rm -d -p 4444:4444 -v /dev/shm:/dev/shm selenium/standalone-chrome:3
    ```
    
    then start the script with:
    
    ```
    SELENIUM_REMOTE_URL=http://localhost:4444/wd/hub node ./apps/www/test/system/google-search.js
    ```
    
    Cool

---
+++ apps/www/test/system/google-search.js
@@ -0,0 +1,9 @@
+const { Builder, By, Key, until } = require('selenium-webdriver')
+
+const driver = new Builder().forBrowser('chrome').build()
+
+driver
+  .get('http://www.google.com/ncr')
+  .then(_ => driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN))
+  .then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 1000))
+  .then(_ => driver.quit())
