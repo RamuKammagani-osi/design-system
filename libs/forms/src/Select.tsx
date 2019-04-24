99f53c349a48bc83057fe2270f27a19ed54ecea5
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-04-23 18:23:30 -0700

    fix(core,forms): fix misc form style nits (#580)
    * fix(core): style focused form component
    
    * fix(forms): style active radio/checkbox
    
    * fix(forms): fix padding radio/checkbox ctrl
    
    * fix(forms): fix disabled Radio/Checkbox hovers
    
    * remove SelectList
    
    * start customizing select
    
    * update styles in Select
    
    * fix cursor on Select:container
    
    * fix missing types

---
+++ libs/forms/src/Select.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import ReactSelect from 'react-select'
+import DS from '@cwds/core'
+
+const Select: React.FunctionComponent<{}> = props => <ReactSelect {...props} />
+
+console.log(DS)
+
+Select.defaultProps = {
+  styles: {
+    control: (base: any, { isDisabled, isFocused, theme }: any) => {
+      return {
+        ...base,
+        borderColor: isDisabled
+          ? theme.colors.neutral10
+          : isFocused
+          ? DS.themeColors.accent
+          : theme.colors.neutral20,
+        boxShadow: isFocused ? `0 0 0 .2rem rgba(96, 25, 116, 0.25)` : null,
+        cursor: isDisabled ? 'default' : 'pointer',
+        '&:hover': {
+          borderColor: isFocused
+            ? DS.themeColors.accent
+            : theme.colors.neutral30,
+        },
+      }
+    },
+    option: (base: any, { isDisabled }: any) => ({
+      ...base,
+      cursor: isDisabled ? 'default' : 'pointer',
+    }),
+  },
+  theme: (theme: any) => ({
+    ...theme,
+    borderRadius: '0.175rem',
+    colors: {
+      ...theme.colors,
+      primary: DS.themeColors.primary,
+      primary25: DS.themeColors.breath,
+      primary50: '#ccf2fa',
+    },
+  }),
+}
+
+export default Select
