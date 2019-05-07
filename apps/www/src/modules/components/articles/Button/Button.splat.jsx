3fe55810b814c52e4ac4162e20858d0a9eb96017
Danny Eck <30477263+eckdanny-osi@users.noreply.github.com>
2019-05-07 11:35:44 -0700

    fix(core): fix disabled IconButton color (#615)
    * fix(core): fix disabled IconButton color
    
    * chore(www): add more Button/IconButton splats

--- apps/www/src/modules/components/articles/Button/Button.splat.jsx
+++ apps/www/src/modules/components/articles/Button/Button.splat.jsx
@@ -1,19 +1,39 @@
 import React from 'react'
-import { Card, CardTitle, CardBody, CardHeader, Button } from '@cwds/components'
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardHeader,
+  Button,
+  Icon,
+  IconButton,
+} from '@cwds/components'
 import { SplatFactory } from '@cwds/docs'
 
-const PROP_OPTIONS = [
-  ['primary', [undefined, true]],
+const commonOptions = [
   ['size', ['sm', 'md', 'lg']],
   ['active', [undefined, true]],
   ['className', [undefined, 'focus']],
-  ['children', ['My Button']],
   ['disabled', [undefined, true]],
 ]
 
-const ButtonSplat = props => (
+const buttonOptions = [
+  ['primary', [undefined, true]],
+  ['children', ['My Button']],
+  ...commonOptions,
+]
+
+const myIconElement = <Icon name="bell" size="sm" />
+
+const iconButtonOptions = [
+  ['icon', [undefined, 'cog', myIconElement]],
+  ['aria-label', ['some label']],
+  ...commonOptions,
+]
+
+const spacedSplat = Component => props => (
   <div className="mb-2">
-    <Button {...props} />
+    <Component {...props} />
   </div>
 )
 
@@ -25,7 +45,21 @@ export default () => {
           <CardTitle>Buttons</CardTitle>
         </CardHeader>
         <CardBody>
-          <SplatFactory component={ButtonSplat} options={PROP_OPTIONS} />
+          <SplatFactory
+            component={spacedSplat(Button)}
+            options={buttonOptions}
+          />
+        </CardBody>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>IconButtons</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <SplatFactory
+            component={spacedSplat(IconButton)}
+            options={iconButtonOptions}
+          />
         </CardBody>
       </Card>
     </div>
