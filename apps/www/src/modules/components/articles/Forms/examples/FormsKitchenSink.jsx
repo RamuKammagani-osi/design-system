7dfe71c3a249dc90b90efed2603b7620c67c02b0
Danny Eck <danny.eck@osi.ca.gov>
2019-05-09 00:31:01 -0700

    more forminess

--- apps/www/src/modules/components/articles/Forms/examples/FormsKitchenSink.jsx
+++ apps/www/src/modules/components/articles/Forms/examples/FormsKitchenSink.jsx
@@ -2,10 +2,12 @@ import React, { Component, Fragment } from 'react'
 import { Formik } from 'formik'
 import {
   Button,
+  ButtonGroup,
   Card,
   CardBody,
   CardHeader,
   CardTitle,
+  CardFooter,
   CheckboxBank,
   DatePicker,
   Form,
@@ -38,156 +40,164 @@ class KitchenSink extends Component {
         <CardHeader>
           <div className="d-flex justify-content-between align-items-center">
             <CardTitle>My Form</CardTitle>
-            <div>
-              <Button
-                size="sm"
-                active={this.state.isVisibleFormData}
-                onClick={this.toggleIsVisibleFormData}
-              >
-                Form State
-              </Button>
+            <div className="d-flex align-items-center">
+              <div className="mr-2">Form State</div>
+              <ButtonGroup>
+                <Button
+                  size="sm"
+                  active={this.state.isVisibleFormData}
+                  onClick={() => this.setState({ isVisibleFormData: true })}
+                >
+                  Show
+                </Button>
+                <Button
+                  size="sm"
+                  active={!this.state.isVisibleFormData}
+                  onClick={() => this.setState({ isVisibleFormData: false })}
+                >
+                  Hide
+                </Button>
+              </ButtonGroup>
             </div>
           </div>
         </CardHeader>
-        <CardBody>
-          <Formik
-            initialValues={{
-              name: '',
-              age: '',
-              tel: '',
-              likes: [],
-              outlook: '',
-              pizzaToppings: [],
-              satisfaction: '',
-              dob: '',
-            }}
-            onSubmit={() => {}}
-            render={props => (
-              <Fragment>
-                <Form onSubmit={props.handleSubmit}>
-                  <FormField
-                    required
-                    id="kitchen-sink-form__name"
-                    name="name"
-                    label="Name"
-                    placeholder="Amos Moses"
-                    autoComplete="off"
-                    Component={Input}
-                    value={props.values.name}
-                    touched={props.touched.name}
-                    error={props.errors.name}
-                    onChange={(event, newValue) =>
-                      props.setFieldValue('name', event.target.value)
-                    }
-                    onBlur={() => props.setFieldTouched('name')}
-                  />
-                  <FormField
-                    required
-                    name="age"
-                    label="Age"
-                    id="kitchen-sink-form__age"
-                    max="99"
-                    min="0"
-                    type="number"
-                    placeholder="23"
-                    Component={Input}
-                    value={props.values.age}
-                    touched={props.touched.age}
-                    error={props.errors.age}
-                    onChange={(event, newValue) =>
-                      props.setFieldValue('age', event.target.value)
-                    }
-                    onBlur={() => props.setFieldTouched('age')}
-                  />
-                  <FormField
-                    name="tel"
-                    label="Telephone"
-                    id="kitchen-sink-form__tel"
-                    Component={InputMask}
-                    mask={MASKS.PHONE_MASK}
-                    unmask={d => d.replace(/[^\d]/gi, '')}
-                    showMask
-                    value={props.values.tel}
-                    touched={props.touched.tel}
-                    error={props.errors.tel}
-                    onChange={(event, newValue) => {
-                      props.setFieldValue('tel', newValue)
-                    }}
-                    onBlur={() => props.setFieldTouched('tel')}
-                  />
-                  <FormField
-                    name="likes"
-                    id="kitchen-sink-form__likes"
-                    label="Likes"
-                    Component={CheckboxBank}
-                    options={LIKE_OPTIONS}
-                    value={props.values.likes}
-                    touched={props.touched.likes}
-                    error={props.errors.likes}
-                    onChange={(event, newValue) =>
-                      props.setFieldValue('likes', newValue)
-                    }
-                    onBlur={() => props.setFieldTouched('likes')}
-                  />
-                  <FormField
-                    name="outlook"
-                    id="kitchen-sink-form__outlook"
-                    label="Outlook"
-                    Component={RadioGroup}
-                    options={OUTLOOK_OPTIONS}
-                    value={props.values.outlook}
-                    touched={props.touched.outlook}
-                    error={props.errors.outlook}
-                    onChange={(event, newValue) =>
-                      props.setFieldValue('outlook', newValue)
-                    }
-                    onBlur={() => props.setFieldTouched('outlook')}
-                  />
-                  <FormField
-                    name="pizzaToppings"
-                    id="kitchen-sink-form__pizza-toppings"
-                    label="Pizza Toppings"
-                    Component={Select}
-                    formatValue={selectOptionsToState}
-                    parse
-                    isMulti
-                    options={PIZZA_TOPPING_OPTIONS}
-                    value={props.values.pizzaToppings}
-                    touched={props.touched.pizzaToppings}
-                    error={props.errors.pizzaToppings}
-                    onChange={(event, newValue, selectArgs) => {
-                      props.setFieldValue('pizzaToppings', newValue)
-                    }}
-                    onBlur={() => props.setFieldTouched('pizzaToppings')}
-                  />
-                  <FormField
-                    id="kitchen-sink-form__satisfaction"
-                    name="satisfaction"
-                    label="Satisfaction"
-                    Component={Select}
-                    options={SERVICE_RATING_OPTIONS}
-                    value={props.values.satisfaction}
-                    formatValue={selectOptionsToState}
-                    touched={props.touched.satisfaction}
-                    error={props.errors.satisfaction}
-                    onChange={(event, newValue, selectArgs) => {
-                      props.setFieldValue('satisfaction', newValue)
-                    }}
-                    onBlur={() => props.setFieldTouched('satisfaction')}
-                  />
-                  <FormField
-                    name="dob"
-                    label="Date of Birth"
-                    Component={DatePicker}
-                    value={props.values.dob}
-                    touched={props.touched.dob}
-                    error={props.errors.dob}
-                    onChange={(event, newValue) =>
-                      props.setFieldValue('dob', newValue)
-                    }
-                    onBlur={() => props.setFieldTouched('dob')}
-                  />
-                </Form>
+        <Formik
+          initialValues={{
+            name: '',
+            age: '',
+            tel: '',
+            likes: [],
+            outlook: '',
+            pizzaToppings: [],
+            satisfaction: '',
+            dob: '',
+          }}
+          onSubmit={() => {}}
+          render={props => (
+            <Form onSubmit={props.handleSubmit} noValidate>
+              <CardBody>
+                <FormField
+                  required
+                  id="kitchen-sink-form__name"
+                  name="name"
+                  label="Name"
+                  placeholder="Amos Moses"
+                  autoComplete="off"
+                  Component={Input}
+                  value={props.values.name}
+                  touched={props.touched.name}
+                  error={props.errors.name}
+                  onChange={(event, newValue) =>
+                    props.setFieldValue('name', event.target.value)
+                  }
+                  onBlur={() => props.setFieldTouched('name')}
+                />
+                <FormField
+                  required
+                  name="age"
+                  label="Age"
+                  id="kitchen-sink-form__age"
+                  max="99"
+                  min="0"
+                  type="number"
+                  placeholder="23"
+                  Component={Input}
+                  value={props.values.age}
+                  touched={props.touched.age}
+                  error={props.errors.age}
+                  onChange={(event, newValue) =>
+                    props.setFieldValue('age', event.target.value)
+                  }
+                  onBlur={() => props.setFieldTouched('age')}
+                />
+                <FormField
+                  name="tel"
+                  label="Telephone"
+                  id="kitchen-sink-form__tel"
+                  Component={InputMask}
+                  mask={MASKS.PHONE_MASK}
+                  unmask={d => d.replace(/[^\d]/gi, '')}
+                  showMask
+                  value={props.values.tel}
+                  touched={props.touched.tel}
+                  error={props.errors.tel}
+                  onChange={(event, newValue) => {
+                    props.setFieldValue('tel', newValue)
+                  }}
+                  onBlur={() => props.setFieldTouched('tel')}
+                />
+                <FormField
+                  name="likes"
+                  id="kitchen-sink-form__likes"
+                  label="Likes"
+                  Component={CheckboxBank}
+                  options={LIKE_OPTIONS}
+                  value={props.values.likes}
+                  touched={props.touched.likes}
+                  error={props.errors.likes}
+                  onChange={(event, newValue) =>
+                    props.setFieldValue('likes', newValue)
+                  }
+                  onBlur={() => props.setFieldTouched('likes')}
+                />
+                <FormField
+                  name="outlook"
+                  id="kitchen-sink-form__outlook"
+                  label="Outlook"
+                  Component={RadioGroup}
+                  options={OUTLOOK_OPTIONS}
+                  value={props.values.outlook}
+                  touched={props.touched.outlook}
+                  error={props.errors.outlook}
+                  onChange={(event, newValue) =>
+                    props.setFieldValue('outlook', newValue)
+                  }
+                  onBlur={() => props.setFieldTouched('outlook')}
+                />
+                <FormField
+                  name="pizzaToppings"
+                  id="kitchen-sink-form__pizza-toppings"
+                  label="Pizza Toppings"
+                  Component={Select}
+                  formatValue={selectOptionsToState}
+                  parse
+                  isMulti
+                  options={PIZZA_TOPPING_OPTIONS}
+                  value={props.values.pizzaToppings}
+                  touched={props.touched.pizzaToppings}
+                  error={props.errors.pizzaToppings}
+                  onChange={(event, newValue, selectArgs) => {
+                    props.setFieldValue('pizzaToppings', newValue)
+                  }}
+                  onBlur={() => props.setFieldTouched('pizzaToppings')}
+                />
+                <FormField
+                  id="kitchen-sink-form__satisfaction"
+                  name="satisfaction"
+                  label="Satisfaction"
+                  Component={Select}
+                  options={SERVICE_RATING_OPTIONS}
+                  value={props.values.satisfaction}
+                  formatValue={selectOptionsToState}
+                  touched={props.touched.satisfaction}
+                  error={props.errors.satisfaction}
+                  onChange={(event, newValue, selectArgs) => {
+                    props.setFieldValue('satisfaction', newValue)
+                  }}
+                  onBlur={() => props.setFieldTouched('satisfaction')}
+                />
+                <FormField
+                  name="dob"
+                  label="Date of Birth"
+                  Component={DatePicker}
+                  value={props.values.dob}
+                  touched={props.touched.dob}
+                  error={props.errors.dob}
+                  onChange={(event, newValue) =>
+                    props.setFieldValue('dob', newValue)
+                  }
+                  onBlur={() => props.setFieldTouched('dob')}
+                />
                 {this.state.isVisibleFormData && (
                   <div>
                     <CodeBlock language="json" className="mb-0">
@@ -195,10 +205,15 @@ class KitchenSink extends Component {
                     </CodeBlock>
                   </div>
                 )}
-              </Fragment>
-            )}
-          />
-        </CardBody>
+              </CardBody>
+              <CardFooter>
+                <Button primary type="submit">
+                  Submit
+                </Button>
+              </CardFooter>
+            </Form>
+          )}
+        />
       </Card>
     )
   }
